60ba0525a831c4136902f1bc7a391eb85beb7b3e
Sacshamm <98898789+Sacshamm@users.noreply.github.com>
2022-08-09 21:20:09 +0530

    Create 037.Defanging an IP Address.cpp

---
+++ Strings/037.Defanging an IP Address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res;
+        for(int  i=0; i<address.length();i++){
+            if(address[i]=='.'){
+                res+= "[.]";
+            }
+            else res+=address[i];
+        }
+        return res;
+    }
+};
