10fd5b2d4ec2c1fdaf79e8cb314e72dea425ddab
Sacshamm <98898789+Sacshamm@users.noreply.github.com>
2022-09-05 09:51:18 +0530

    Create 053.Remove Nth Node From End of List.cpp

---
+++ Linked List/053.Remove Nth Node From End of List.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head->next==0) return 0;
+        
+        int len=0;
+        ListNode *temp=head;
+        while(temp!=0){
+            temp=temp->next;
+            len++;
+        }
+        
+        int node=len-n;
+        if(node==0) return head->next;//if  n== total length
+        
+         ListNode*prev=head;
+        for(int i=1;i<node;i++){
+           prev=prev->next;
+        }
+        ListNode*del=prev->next;
+        prev->next=del->next;
+        delete del;
+        return head;
+        
+//         ListNode *fast = head, *slow = head;
+// 	    while(n--) {
+//            fast = fast -> next;//iterate first n nodes using fast
+//         }
+        
+// 	    if(!fast) return head -> next;// if fast is already null, it means we have to delete head itself. So,just           return next of head
+        
+// 	     while(fast -> next){// iterate till fast reaches the last node of list
+// 		    fast = fast -> next, slow = slow -> next;      
+//         }
+         
+// 	      slow -> next = slow -> next -> next; // remove the nth node from last
+// 	      return head;
+    }
+        
+};
