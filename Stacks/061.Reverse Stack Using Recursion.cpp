0627f90ecfe007c985b2f11f2ce7a2c2a5e150cd
Sacshamm <98898789+Sacshamm@users.noreply.github.com>
2022-09-11 20:29:02 +0530

    Update 061.Reverse Stack Using Recursion.cpp

--- Stacks/061.Reverse Stack Using Recursion.cpp
+++ Stacks/061.Reverse Stack Using Recursion.cpp
@@ -1,3 +1,6 @@
+
+//[1 2 3   to 3 2 1 
+
 void insertAtBottom(stack<int> &stack,int x){
     if(stack.empty()){
         stack.push(x);
@@ -21,3 +24,7 @@ void reverseStack(stack<int> &stack) {
     
     insertAtBottom(stack,num);
 }
+// firstly reverse() call will execute until stack gets empty and have 1 2 3 in its stack space
+//After that insertAtBottom() call will execute with 1 as top 
+//bottom function will again pop until stack gets empty and at last insert 1 then return with 2 aand then 3
+//top became 3
