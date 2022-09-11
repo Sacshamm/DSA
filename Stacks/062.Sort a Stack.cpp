1745af0db99f05a8644b5a4ad05b31a8822c4b9d
Sacshamm <98898789+Sacshamm@users.noreply.github.com>
2022-09-11 22:38:49 +0530

    Update 062.Sort a Stack.cpp

--- Stacks/062.Sort a Stack.cpp
+++ Stacks/062.Sort a Stack.cpp
@@ -1,12 +1,23 @@
+/*Let num = 6
+    |9|
+  -------
+    |5|
+    |2|
+when 5(which is s.top())<6 then push 6
+*/
+
+
+
+
 void sorted(stack<int> &s, int num){
-    if(s.empty() || (!s.empty()) && s.top()<num){
+    if(s.empty() || (!s.empty()) && s.top()<num){// checking either if stack is empty then push. Or before checking s.top() with num is our stack is not empty
         s.push(num);
         return;
     }
     
     int temp=s.top();
     s.pop();
-    sorted(s,num);
+    sorted(s,num);//recursive call
     s.push(temp);
 }
 
@@ -18,7 +29,7 @@ void SortedStack :: sort()
    int num=s.top();
    s.pop();
    
-   sort();
+   sort();//recursive call 
    
    sorted(s,num);
 }
